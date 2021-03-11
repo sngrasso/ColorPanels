75722617f0da43607715b291b220dedf3cbf7033
Stephanie Grasso <sngrasso@dons.usfca.edu>
2021-03-11 10:54:58 -0800

    some revisions

--- sketch.js
+++ sketch.js
@@ -10,30 +10,39 @@
  * includes a replay button, help button, and
 
  ***********************************************************************************/
+// Object global variables
 let panelTimer;
 let gameTimer;
 let clickButton;
-let elapsedSeconds = 0;
-let score = 0;
+
+// board and tallies
 let boardDimensions = 3;
-let table = [];
+let tally = 0;
+
+// random globals for panel choice and colors
 let r1 = 0;
 let r2 = 0;
+let table = [];
 let possibilities = [0, 1, 2];
 let colorChoices = ["#3434eb", "#ed37d2", "#f0b71d", "#b479e8"]
 let colorPicker = 0;
+
+// offsets
+let titleOffset = 50;
 
 // Setup code goes here
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  // create timers
   panelTimer = new Timer(500);
   gameTimer = new Timer(15000);
-  clickButton = new Clickable();
-
-  initButton();
-
+  // start timer
   panelTimer.start();
+
+  // make buttons
+  clickButton = new Clickable();
+  initButton();
 
   for (let i = 0; i < boardDimensions; i++){
       let box = []
@@ -49,18 +58,22 @@ function setup() {
 
 // Draw code goes here
 function draw() {
-  background(0);
-  // draws the replay button
-  clickButton.draw();
-  if (!gameTimer.expired()) {
-      updatePanel();
-  }
-  else {
-      table[r1][r2] = 0;
-  }
+    background(0);
 
-  fill(255);
-  drawBoard();
+    // draws title
+    drawTitle();
+
+    // draws the replay button
+    clickButton.draw();
+
+    // update new random panel
+    checkRunTime();
+
+    //draw board
+    drawBoard();
+
+    //draw tally
+    drawTally();
 }
 
 // initButton() -- initialize buttons
@@ -85,19 +98,48 @@ function drawBoard() {
                 fill(255);
             }
             ellipse((width / 2) + ((j - .75) * 200), (height / 2) +  ((i - 1) * 80), 100, 70);
+            fill(255)
+            ellipse((width / 2) + ((j - .75) * 200), (height / 2) +  ((i - 1) * 80), 50, 35);
         }
     }
 }
+
+// drawTally() -- draws lines to show the tally of the amount of times "replay" was clicked
+function drawTally() {
+    // this is the tally
+    text("tally:", titleOffset, height - 125)
+
+    // fix stroke settings
+    strokeWeight(4);
+    stroke(255);
+
+    // loop through to make tallies
+    for (let i = 0; i < tally; i++) {
+        line( (i * 20) + 100, height - 150, (i * 20) + 100, height - 100);
+    }
+
+    // fix stroke back
+    noStroke();
+}
+
+// drawTitle() -- draws the title of piece at the top
+function drawTitle() {
+    fill("red");
+    textSize(30);
+    text("[Color Panel] : Does nothing but change color THATS IT!!", width / 2 + titleOffset, titleOffset);
+}
+
 // randomizePanel() -- randomly chooses a panel in the table to color
 function randomizePanel() {
     // get random panel to color
-    r1 = random(possibilities);
-    r2 = random(possibilities);
+    r1 = round(random(boardDimensions - 1));
+    r2 = round(random(boardDimensions - 1));
     // set in table to true
     table[r1][r2] = 1;
     console.log("r1: " + r1 + " r2: " + r2);
 }
 
+// setRandomColor() -- sets a random color of what is available
 function setRandomColor() {
     colorPicker = round(random(colorChoices.length - 1))
     console.log("Color picker chose: colorChoices[" + colorPicker + "] -> " + colorChoices[colorPicker]);
@@ -105,28 +147,44 @@ function setRandomColor() {
 
 // updatePanel() -- Looks for elapsed time
 function updatePanel() {
-  if( panelTimer.expired() ) {
-    table[r1][r2] = 0;
-    // set new "mole"
-    randomizePanel();
-    // set random color
-    setRandomColor();
-    // reset timer
-  	panelTimer.start();
-  }
+    if(panelTimer.expired()) {
+        table[r1][r2] = 0;
+        // set new "mole"
+        randomizePanel();
+        // set random color
+        setRandomColor();
+        // reset timer
+        panelTimer.start();
+    }
 }
 
+// checkRunTime() -- checks to make sure the loop timer is still running if so update new panel
+function checkRunTime() {
+    if (!gameTimer.expired()) {
+        updatePanel();
+    }
+    else {
+        // reset the last panel
+        table[r1][r2] = 0;
+    }
+}
+
+// button non hover settings
 buttonOutside = function () {
     this.color = "#FFFFFF";
     this.textColor = "#000000";
+    this.textSize = 20;
 }
 
+// button hover settings
 buttonHover = function () {
     this.color = "#FF0000";
-    this.textColor = "#FFFFFF";
 }
 
 // for replay clickable button
 replay = function () {
+    // reset game timer
     gameTimer.start();
+    // update tally
+    tally = tally + 1;
 }
